067f0ccbccf3693c7135a75a41c9011ca42e026f
Haroon-S <harryshahid420@gmail.com>
2022-05-28 00:38:44 +0500

    setting roles in controller

--- app/Http/Controllers/Auth/RegisteredUserController.php
+++ app/Http/Controllers/Auth/RegisteredUserController.php
@@ -48,7 +48,7 @@ class RegisteredUserController extends Controller
         event(new Registered($user));
 
         Auth::login($user);
-        $user->attachRole($admin);
+        $user->attachRole('student');
 
         return redirect(RouteServiceProvider::HOME);
     }
