6b721a07436b8198a9648bb2e80aa26b177a4364
Haroon-S <harryshahid420@gmail.com>
2022-05-29 00:22:22 +0500

    3 dashboard for teacher student and admin roles set

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        if(Auth::user()->hasRole('student')){
+             return view('studentdashboard');
+        }elseif(Auth::user()->hasRole('teacher')){
+             return view('teacherdashboard');
+        }elseif(Auth::user()->hasRole('admin')){
+         return view('dashboard');
+    }
+    }
+}
