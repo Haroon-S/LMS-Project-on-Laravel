e8bd2c2014d00f60c98038fe76b233980d0de4f9
Haroon-S <harryshahid420@gmail.com>
2022-05-29 18:26:46 +0500

    Courses migration added

---
+++ database/migrations/2022_05_29_125647_create_courses_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('role_id');
+            $table->foreign('role_id')->references('id')->on('roles');
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->string('course_title');
+            $table->string('course_discription');
+            $table->string('course_thumbnail');
+            $table->string('course_videos');
+            $table->string('teacher_name');
+            $table->integer('number_of_students');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
