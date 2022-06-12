e7af0e1244ce7281a633587df0385613f25bf5e8
Haroon-S <harryshahid420@gmail.com>
2022-06-12 22:25:04 +0500

    enrolment

---
+++ app/Http/Controllers/EnrolmentControllerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\EnrolmentController;
+use Illuminate\Http\Request;
+
+class EnrolmentControllerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\EnrolmentController  $enrolmentController
+     * @return \Illuminate\Http\Response
+     */
+    public function show(EnrolmentController $enrolmentController)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\EnrolmentController  $enrolmentController
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(EnrolmentController $enrolmentController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\EnrolmentController  $enrolmentController
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, EnrolmentController $enrolmentController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\EnrolmentController  $enrolmentController
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(EnrolmentController $enrolmentController)
+    {
+        //
+    }
+}
